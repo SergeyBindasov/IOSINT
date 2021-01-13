db859935eac837ea9bbe48ade71ec450ada44eb6
Sergey Bindasov <bindasov.sergey@gmail.com>
2021-01-13 16:27:03 +0300

    готовая домашка, тренируюсь с гитом

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -10,28 +10,77 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
+    
+    var backgroundTask: UIBackgroundTaskIdentifier = .invalid
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
+        
+    
+        let tabBarController = UITabBarController()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let storyboardVCFeed = storyboard.instantiateViewController(withIdentifier: "feed")
+        let storyboardVCLogin = storyboard.instantiateViewController(withIdentifier: "login")
+        let FeedNavigationController = UINavigationController(rootViewController: storyboardVCFeed)
+        
+        storyboardVCFeed.title = "Feed"
+        if #available(iOS 13.0, *) {
+            storyboardVCFeed.tabBarItem.image = UIImage(systemName: "house.fill")
+        } else {
+            // Fallback on earlier versions
+        }
+        storyboardVCLogin.title = "Profile"
+        if #available(iOS 13.0, *) {
+            storyboardVCLogin.tabBarItem.image = UIImage(systemName: "person.fill")
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        tabBarController.viewControllers = [FeedNavigationController, storyboardVCLogin]
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("ResignActive")
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        endBackgroundTask()
+        print("BecomeActive")
     }
-
-
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        registerBackgroundTask()
+        // 26 секунд
+        print("EnterBackground")
+    }
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("EnterForeground")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("WillTerminate")
+    }
+    
+    func registerBackgroundTask() {
+      backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
+        self?.endBackgroundTask()
+      }
+      assert(backgroundTask != .invalid)
+    }
+    
+    func endBackgroundTask() {
+      print("Background task ended.")
+      UIApplication.shared.endBackgroundTask(backgroundTask)
+      backgroundTask = .invalid
+    }
 }
 
